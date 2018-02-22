420ab395ba14b47de612ddf572adfe3d8fa0638a
Sebastian Webber <sebastian@swebber.me>
2018-02-22 17:38:32 -0300

    checkking commands in the cli

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -14,6 +15,7 @@ const currentVersion = "0.2.0"
 var (
 	portNumber  *int
 	interval    *time.Duration
+	timeout     *time.Duration
 	command     *string
 	showVersion *bool
 )
@@ -28,6 +30,7 @@ func processArgs() {
 	portNumber = flag.Int("port", 0, "port name to check")
 	command = flag.String("command", "", "port name to check")
 	interval = flag.Duration("interval", 1*time.Second, "time for each check")
+	timeout = flag.Duration("timeout", 5*time.Second, "timeout for a port or command check")
 	showVersion = flag.Bool("version", false, "Print version information and quit")
 	flag.Parse()
 
@@ -35,20 +38,44 @@ func processArgs() {
 		fmt.Printf("wait4 v%s\n", currentVersion)
 		os.Exit(0)
 	}
+
 }
 
 func main() {
 
 	processArgs()
 
+	var (
+		checkPort    bool
+		checkCommand bool
+		err          error
+	)
 	c := time.Tick(*interval)
-	for range c {
-		check, _ := lib.PortCheck(*portNumber, 5*time.Second)
-		fmt.Printf("Waiting for %d port...\n", *portNumber)
-		if check {
-			fmt.Printf("%d port is listenning!\n", *portNumber)
-			break
+
+	if *portNumber > 0 {
+		for range c {
+			checkPort, _ = lib.PortCheck(*portNumber, *timeout)
+			log.Printf("Waiting for %d port...\n", *portNumber)
+			if checkPort {
+				log.Printf("%d port is listenning!\n", *portNumber)
+				break
+			}
 		}
 	}
 
+	if *command != "" {
+		var out []byte
+		for range c {
+			checkCommand, out, err = lib.CmdCheck(*command)
+			if err != nil {
+				log.Printf("%s returns:\n%s%s\n", *command, string(out), err.Error())
+			}
+			if checkCommand {
+				log.Printf("Command '%s' returns without error.\n", *command)
+				break
+			}
+		}
+	}
+
+	log.Println("Done.")
 }
